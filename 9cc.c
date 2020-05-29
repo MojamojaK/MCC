755b790c24a9ec9e85d1f914d393bc3ca84f1f32
Kazuki <kazukifu@gmail.com>
2020-05-29 18:08:20 +0900

    Implement Support for mul/div

--- 9cc.c
+++ 9cc.c
@@ -66,6 +66,20 @@ bool at_eof() {
 	return token->kind == TK_EOF;
 }
 
+static const char reserved_characters[] = {
+    '+', '-', '*', '/', '(', ')'
+};
+
+bool is_reserved(const char c) {
+    const int n = sizeof(reserved_characters) / sizeof(char);
+    for (int i = 0; i < n; i++) {
+        if (c == reserved_characters[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 Token *new_token(const TokenKind kind, Token *cur, char *str) {
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
@@ -85,7 +99,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 		
-		if (*p == '+' || *p == '-') {
+		if (is_reserved(*p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -103,6 +117,107 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(const NodeKind kind, Node *lhs, Node *rhs) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(const int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("\tpush\t%d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+    printf("\tpop\trdi\n");
+    printf("\tpop\trax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("\tadd\trax,\trdi\n");
+            break;
+        case ND_SUB:
+            printf("\tsub\trax,\trdi\n");
+            break;
+        case ND_MUL:
+            printf("\timul\trax,\trdi\n");
+            break;
+        case ND_DIV:
+            printf("\tcqo\n");
+            printf("\tidiv\trdi\n");
+            break;
+    }
+
+    printf ("\tpush\trax\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		fprintf(stderr, "Insufficient Parameters\n");
@@ -112,22 +227,18 @@ int main(int argc, char **argv) {
 	user_input = argv[1];
 
 	token = tokenize(argv[1]);
+    // fprintf(stderr, "Tokenized\n");
+
+    Node *node = expr();
+    // fprintf(stderr, "Node constructed\n");
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 	
-	printf("\tmov\trax,\t%d\n", expect_number());
+    gen(node);	
 	
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("\tadd\trax,\t%d\n", expect_number());
-			continue;
-		}
-		expect('-');
-		printf("\tsub\trax,\t%d\n", expect_number());
-	}
-	
+    printf("\tpop\trax\n");
 	printf("\tret\n");
 	return 0;
 }
